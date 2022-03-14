d63f8d53f3e6cc0bf4d5cf3b3d4cb2d67c095d7b
DELL <rizkiakmal37@gmail.com>
2022-03-14 15:10:18 +0700

    Initial project pertemuan 1 dasar Java

---
+++ src/A_BlokDanKomentar.java
@@ -0,0 +1,14 @@
+public class A_BlokDanKomentar {
+    public static void main(String[] args) {
+        System.out.println("Pemrograman Berorientasi Objek dengan Java");
+        //Contoh komentar 1 baris
+
+        /*
+        Contoh komentar lebih dari satu baris
+        */
+
+        /**
+         * Java Doc --> baris ini untuk mendokemntasikan kode program
+         */
+    }
+}
