92c1c15e95235ccf7806cc9ef764246fbcffd349
DELL <rizkiakmal37@gmail.com>
2022-03-21 14:10:40 +0700

    Mengkonversi nilai masukan dari keyboard ke tipe tertentu menggunakan method bawaan kelas Scanner

---
+++ src/V_InputTypeScannerB.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class V_InputTypeScannerB {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan nama : ");
+        String name = input.nextLine();
+        System.out.println("Masukkan umur : ");
+        int age = input.nextInt();
+        System.out.println("Masukkan tinggi badan : ");
+        double height = input.nextDouble();
+        System.out.println("Sudah Menikah (true/false) : ");
+        boolean married = input.nextBoolean();
+        System.out.println("Nama ; " + name);
+        System.out.println("Umur : " + age);
+        System.out.println("Tinggi Badan : " + height);
+        System.out.println("Sudah Menikah : " + married);
+
+    }
+}
